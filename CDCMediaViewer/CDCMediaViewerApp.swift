c1d577730b0b6e8854cadcafbb8d264cba11731d
Victorola <victoluolatunji@gmail.com>
2024-11-05 01:23:43 +0100

    Initial Commit

---
+++ CDCMediaViewer/CDCMediaViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDCMediaViewerApp.swift
+//  CDCMediaViewer
+//
+//  Created by VickyJay on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CDCMediaViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CDCMediaViewerTests/CDCMediaViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CDCMediaViewerTests.swift
+//  CDCMediaViewerTests
+//
+//  Created by VickyJay on 05/11/2024.
+//
+
+import Testing
+@testable import CDCMediaViewer
+
+struct CDCMediaViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
